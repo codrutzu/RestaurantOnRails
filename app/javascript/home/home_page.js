a1319d448fd33a2ad15898e34dc941d2c98ff8b9
unknown <codandrei12@gmail.com>
2020-07-29 11:57:43 +0300

    Filter section with slider

---
+++ app/javascript/home/home_page.js
@@ -0,0 +1,16 @@
+var slider;
+
+$(document).ready(function() {
+  slider = $("#slider").slider({
+      range: true,
+      min: 0,
+      max: 100,
+      values: [10, 35],
+      slide: function(event, ui) {
+          $("#first-price").html("$ " + ui.values[0]);
+          $("#second-price").html("$ " + ui.values[1]);
+      }
+  });
+  $("#first-price").html("$ " + slider.slider("values")[0]);
+  $("#second-price").html("$ " + slider.slider("values")[1]);
+});
